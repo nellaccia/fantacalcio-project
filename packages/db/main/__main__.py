f91f1104ee9bc9fb8aa4faeaeb04ec1eb9719bdd
Tmarinelli9 <47790366+nellaccia@users.noreply.github.com>
2024-10-10 20:59:05 +0100

    fe: controllo apikey per update, insert e delete

--- packages/db/main/__main__.py
+++ packages/db/main/__main__.py
@@ -1,6 +1,8 @@
 #--web true
 #--kind python:default
 #--param POSTGRES_URL $POSTGRES_URL
+#--param FANTABALUN_API_KEY $FANTABALUN_API_KEY
+
 from dataclasses import dataclass
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -15,9 +17,26 @@ from models.player_season import PlayerSeason
 from models.player_statistic import PlayerStatistic
 from models.current_player_team import CurrentPlayerTeam
 
+def check_authorization(args):
+    query_type = args.get("query")
+    print('query: ' + str(query_type))  
+    if query_type in ["update", "insert", "delete"]:
+        apikey = args.get("api_key", "") 
+                print(f"apikey: {apikey}")
+        FANTABALUN_API_KEY = args.get("FANTABALUN_API_KEY")
+        print(f"FANTABALUN_API_KEY: {FANTABALUN_API_KEY}")
+        if apikey != FANTABALUN_API_KEY:
+            return False
+    return True
+
 
 def main(args):
 
+    if not check_authorization(args): 
+        return {
+            "body": "api key not valid or present"
+        }
+         
     db_url = args.get("POSTGRES_URL")
    
     db = DbConnection()
